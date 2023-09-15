9f811a723e5b032c6a0f3493a691fbc9a6bb69a5
KJW519 <jwkang519@gmail.com>
2023-09-15 10:10:27 +0900

    Word Edit

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -134,7 +134,7 @@ public class WordCRUD implements ICRUD{
                 count++;
             }
             br.close();
-            System.out.println("==> "+ count + "개 로딩 완료!!!");
+            System.out.println("==> "+ count + "개 로딩 완료.");
 
         } catch (IOException e) {
 
